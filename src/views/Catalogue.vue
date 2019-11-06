6b9fd3476b937bf86c750b6d2fe83e85111d7f07
Francesco Moro <francesco.moro@Mac-mini-di-Francesco.local>
2019-11-06 11:59:18 +0100

    test movies list

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,10 +1,49 @@
 <template>
     <div class="catalogue">
         <h1>This is the Catalogue page</h1>
+        <div class="movies-list">
+            <Movie v-for="movie in movies" :key="movies.id" :movie="movie" />
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-}
+    import Movie from '@/components/Movie';
+    import axios from 'axios';
+
+    export default {
+        name: 'catalogue',
+        components: {
+            Movie
+        },
+        data() {
+            return {
+                res: null,
+                movies: []
+            }
+        },
+        methods: {
+            loadMovies: function() {
+                axios
+                    .get('https://api.themoviedb.org/3/movie/popular?api_key=d6aab43d41a49e768563d3c740965ef2')
+                    .then(resp => {
+                        this.res = resp.data.results;
+                        this.res.map(movie => this.movies.push(movie));
+
+                        console.log(this.movies);
+
+                    })
+            }
+        },
+        mounted() {
+            this.loadMovies();
+        }
+    }
 </script>
+
+<style lang="scss">
+    .movies-list {
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
