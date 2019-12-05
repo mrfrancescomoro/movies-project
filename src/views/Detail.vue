6d0950bf145e7db53628abaeacdaf8a691112cf7
Francesco Moro <francesco.moro@Mac-mini-di-Francesco.local>
2019-12-05 14:21:35 +0100

    detail edit

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -6,13 +6,15 @@
         <div class="movie">
             <div class="content">
                 <div class="movie__data">
-                    <small class="movie__data-duration">Duration: {{ this.movie.runtime }} min</small>
                     <h1 class="movie__data-title">{{ this.movie.original_title }}</h1>
-                    <div class="movie__data-cats">
-                        <strong>Genres:</strong>
-                        <span v-for="genre in this.movie.genres">
-                            {{ genre.name }}
-                        </span>
+                    <div class="movie__data-info">
+                        <span class="movie__data-duration">Duration: {{ this.movie.runtime }} min</span>
+                        <div class="movie__data-cats">
+                            <strong>Genres:</strong>
+                            <span v-for="genre in this.movie.genres">
+                                {{ genre.name }}
+                            </span>
+                        </div>
                     </div>
                     <p class="movie__info-description">
                         {{ this.movie.overview}}
@@ -64,66 +66,5 @@ export default {
 </script>
 
 <style lang="scss">
-.detail {
-    position: relative;
-    .bg {
-        position: fixed;
-        top: 0;
-        left: 0;
-        background: $black;
-        @include size(100vw,100vh);
-        z-index: 0;
-        img {
-            @include size(100%,100%);
-            object-fit: cover;
-        }
-        &:after {
-            content: '';
-            position: absolute;
-            top: 0;
-            left: 0;
-            @include size(100%,100%);
-            background: $black;
-            mix-blend-mode:multiply;
-            opacity: 0.85;
-        }
-    }
-    .movie {
-        display: flex;
-        @include size(100%,auto);
-        position: relative;
-        border: 0;
-        padding-top: 30vh;
-        color: $white;
-        margin: 0;
-        &:before {
-            content: '';
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            @include size(100%,40vh);
-            background-image: linear-gradient(to top, $black, transparent);
-        }
-        .content {
-            display: flex;
-            @extend %flexStartBetween;
-        }
-        &__data {
-            width: 25%;
-            &-title {
-                font-size: 3em;
-                line-height: 1em;
-            }
-            &-description {
-                line-height: 1.5em;
-            }
-        }
-        &__poster {
-            width: 15%;
-            img {
-                width: 100%;
-            }
-        }
-    }
-}
+@import '@/assets/scss/components/Detail';
 </style>
